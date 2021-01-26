b09be671c46eec1a7f300956ff039db8a8d5d029
Lorenah <lorayne911@gmail.com>
2021-01-26 15:46:53 +0300

    restructure register page

--- app/src/main/java/com/skylar/beer_lifesaver/RegisterActivity.java
+++ app/src/main/java/com/skylar/beer_lifesaver/RegisterActivity.java
@@ -1,99 +1,185 @@
 package com.skylar.beer_lifesaver;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Patterns;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
-public class RegisterActivity extends AppCompatActivity  {
-    EditText name, email, phone, password;
-    Button register;
-    TextView login;
-    boolean isNameValid, isEmailValid, isPhoneValid, isPasswordValid;
-    TextInputLayout nameError, emailError, phoneError, passError;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = RegisterActivity.class.getSimpleName();
+
+    @BindView(R.id.logoText) TextView mLogoText;
+    @BindView(R.id.nameEditText) EditText mName;
+    @BindView(R.id.emailEditText) EditText mEmail;
+    @BindView(R.id.passwordEditText) EditText mPassword;
+    @BindView(R.id.confirmPasswordEditText)EditText mPasswordConfirm;
+    @BindView(R.id.createUserButton)Button mCreateUser;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private ProgressDialog mAuthProgressDialog;
+    private String mUserName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
 
-        name = findViewById(R.id.name);
-        email = findViewById(R.id.email);
-        phone = findViewById(R.id.phone);
-        password = findViewById(R.id.password);
-        login = findViewById(R.id.login);
-        register = findViewById(R.id.register);
-        nameError = findViewById(R.id.nameError);
-        emailError = findViewById(R.id.emailError);
-        phoneError = findViewById(R.id.phoneError);
-        passError = findViewById(R.id.passError);
+//        Typeface FFF_Tusj = Typeface.createFromAsset(getAssets(), "fonts/FFF_Tusj.ttf");
 
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
-                startActivity(intent);
-            }
-        });
+//        mLogoText.setTypeface(FFF_Tusj);
+
+        mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
+
+        mCreateUser.setOnClickListener(this);
+        createAuthProgressDialog();
+
     }
 
-    public void SetValidation () {
-        // Check for a valid name.
-        if (name.getText().toString().isEmpty()) {
-            nameError.setError(getResources().getString(R.string.name_error));
-            isNameValid = false;
-        } else {
-            isNameValid = true;
-            nameError.setErrorEnabled(false);
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
         }
+    }
 
-        // Check for a valid email address.
-        if (email.getText().toString().isEmpty()) {
-            emailError.setError(getResources().getString(R.string.email_error));
-            isEmailValid = false;
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()) {
-            emailError.setError(getResources().getString(R.string.error_invalid_email));
-            isEmailValid = false;
-        } else {
-            isEmailValid = true;
-            emailError.setErrorEnabled(false);
+    @Override
+    public void onClick(View view) {
+        if (view == mCreateUser) {
+            createNewUser();
         }
+    }
 
-        // Check for a valid phone number.
-        if (phone.getText().toString().isEmpty()) {
-            phoneError.setError(getResources().getString(R.string.phone_error));
-            isPhoneValid = false;
-        } else {
-            isPhoneValid = true;
-            phoneError.setErrorEnabled(false);
+    private void createAuthProgressDialog() {
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading...");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
+    }
+
+    private void createNewUser() {
+        mUserName = mName.getText().toString().trim();
+        final String name = mName.getText().toString().trim();
+        final String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+        String confirmPassword = mPasswordConfirm.getText().toString().trim();
+
+        boolean validEmail = isValidEmail(email);
+        boolean validName = isValidName(name);
+        boolean validPassword = isValidPassword(password, confirmPassword);
+        if (!validEmail || !validName || !validPassword) return;
+
+        mAuthProgressDialog.show();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        mAuthProgressDialog.dismiss();
+
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Authentication successful");
+                            createFirebaseUserProfile(task.getResult().getUser());
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
+    private void createFirebaseUserProfile(final FirebaseUser user) {
+
+        UserProfileChangeRequest addProfileName = new UserProfileChangeRequest.Builder()
+                .setDisplayName(mUserName)
+                .build();
+
+        user.updateProfile(addProfileName)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, user.getDisplayName());
+                        }
+                    }
+
+                });
+    }
+
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };
+    }
+
+    private boolean isValidEmail(String email) {
+        boolean isGoodEmail =
+                (email != null && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        if (!isGoodEmail) {
+            mEmail.setError("Please enter a valid email address");
+            return false;
         }
+        return isGoodEmail;
+    }
 
-        // Check for a valid password.
-        if (password.getText().toString().isEmpty()) {
-            passError.setError(getResources().getString(R.string.password_error));
-            isPasswordValid = false;
-        } else if (password.getText().length() < 6) {
-            passError.setError(getResources().getString(R.string.error_invalid_password));
-            isPasswordValid = false;
-        } else {
-            isPasswordValid = true;
-            passError.setErrorEnabled(false);
+    private boolean isValidName(String name) {
+        if (name.equals("")) {
+            mName.setError("Please enter your name");
+            return false;
         }
+        return true;
+    }
 
-        if (isNameValid && isEmailValid && isPhoneValid && isPasswordValid) {
-
-            Toast.makeText(getApplicationContext(), "Successfully", Toast.LENGTH_SHORT).show();
-
-
+    private boolean isValidPassword(String password, String confirmPassword) {
+        if (password.length() < 6) {
+            mPassword.setError("Please create a password containing at least 6 characters");
+            return false;
+        } else if (!password.equals(confirmPassword)) {
+            mPassword.setError("Passwords do not match");
+            return false;
         }
-
+        return true;
     }
 }
